dfe31c9271b9840563c7e511980cfeb2f4f52715
Reflective <brandon.r.poll@gmail.com>
2022-08-05 16:54:13 -0400

    Post model still not working

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,15 @@ from PIL import Image
 
 # Post will contain the various fields that will be populated by the user when writing
 class Post(models.Model): 
+    # Cascade deletes the authors posts if the user is deleted.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     postMedia = models.ImageField(null=True, blank=True, upload_to="pics")
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # Uses current time from timezone
     date_posted = models.DateTimeField(
         default=timezone.now
-    )  # Uses current time from timezone
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    )  
 
     def __str__(self):
         return self.title
@@ -23,7 +24,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         # overriding save method to resize images over 800 px
-        img = Image.open(Post.postMedia.url)
+        img = Image.open(self.postMedia)
         if img.height > 800 or img.width > 800:
             output_size = (800, 800)
         img.thumbnail(output_size)
