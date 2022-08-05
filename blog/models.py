b8152e770ce280c3439063d3fd40597caa83e621
Reflective <brandon.r.poll@gmail.com>
2022-08-05 19:01:08 -0400

    Pushing bugggsss

--- blog/models.py
+++ blog/models.py
@@ -8,15 +8,14 @@ from PIL import Image
 
 # Post will contain the various fields that will be populated by the user when writing
 class Post(models.Model): 
-    # Cascade deletes the authors posts if the user is deleted.
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     postMedia = models.ImageField(null=True, blank=True, upload_to="pics")
     title = models.CharField(max_length=100)
     content = models.TextField()
-    # Uses current time from timezone
     date_posted = models.DateTimeField(
         default=timezone.now
-    )  
+    )  # Uses current time from timezone
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
@@ -24,11 +23,11 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         # overriding save method to resize images over 800 px
-        img = Image.open(self.postMedia)
+        img = Image.open(self.postMedia.path)
         if img.height > 800 or img.width > 800:
             output_size = (800, 800)
-        img.thumbnail(output_size)
-        return img.save(self.postMedia)
+            img.thumbnail(output_size)
+        img.save(self.postMedia.path)
             
 
     def get_absolute_url(self):
